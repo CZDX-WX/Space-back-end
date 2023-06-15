e10017ee773b7eee126a82bc652e2b224d9ae0bc
Wei Xin <czdxwx@gmail.com>
2023-06-16 05:54:45 +0800

    第一题提交
    初始化spring工程

---
+++ src/test/java/com/czdxwx/space/SpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czdxwx.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
